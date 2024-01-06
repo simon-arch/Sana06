c2bac5c992a71fbe22843370468b1aca3d328fee
Артем Ващенко <ipz231_vai@student.ztu.edu.ua>
2024-01-06 14:05:44 +0200

    Added set/get to class Person. Implemented inheritance to every class

--- Inheritance/ClassLibrary.cs
+++ Inheritance/ClassLibrary.cs
@@ -1,56 +1,97 @@
 ﻿namespace Inheritance
 {
-    internal class ClassLibrary
+    public class Date
     {
-        public class Date
+        protected int Day;
+        protected int Month;
+        protected int Year;
+        public Date(int day, int month, int year)
         {
-            protected int Day;
-            protected int Month;
-            protected int Year;
-            public Date(int day, int month, int year)
-            {
-                Day = day;
-                Month = month;
-                Year = year;
-            }
-            public void SetYear(int year) {if (year > 0) Year = year;}
-            public void SetMonth(int month) {if (month > 0 && month <= 12) Month = month;}
-            public void SetDay(int day) {if (day > 0 && day <= 366)  Day = day;}
-            public int GetDay() {return Day;}
-            public int GetMonth() {return Month;}
-            public int GetYear() {return Year;}
+            Day = day;
+            Month = month;
+            Year = year;
         }
-        public class Person
+        public void SetYear(int year) {if (year > 0) Year = year;}
+        public void SetMonth(int month) {if (month > 0 && month <= 12) Month = month;}
+        public void SetDay(int day) {if (day > 0 && day <= 366)  Day = day;}
+        public int GetDay() {return Day;}
+        public int GetMonth() {return Month;}
+        public int GetYear() {return Year;}
+    }
+    public class Person
+    {
+        protected string Name;
+        protected string Surname;
+        protected string Patronymic;
+        protected Date BirthDate;
+        public Person(string name, string surname, string patronymic, Date birthDate)
         {
-            protected string name;
-            protected string surname;
-            protected string patronymic;
-            protected Date birthDate;
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            BirthDate = birthDate;
         }
-        public class Enrollee : Person
+        public void SetName(string name) {if (name.Length > 0) Name = name;}
+        public void SetSurname(string surname) {if (surname.Length > 0) Surname = surname;}
+        public void SetPatronymic(string patronymic) {if (patronymic.Length > 0) Patronymic = patronymic;}
+        public void SetBirthDate(Date date) {BirthDate = date;}
+        public string GetName() {return Name;}
+        public string GetSurname() {return Surname;}
+        public string GetPatronymic() {return Patronymic;}
+        public Date GetBirthDate() {return BirthDate;}
+    }
+    public class Enrollee : Person
+    {
+        protected int ZnoMark;
+        protected int SchoolMark;
+        protected string University;
+        public Enrollee(string name, string surname, string patronymic, Date birthDate, int znoMark, int schoolMark, string university) : 
+            base(name, surname, patronymic, birthDate)
         {
-            protected int znoMark;
-            protected int schoolMark;
-            protected string university;
+            ZnoMark = znoMark;
+            SchoolMark = schoolMark;
+            University = university;
         }
-        public class Student : Person
+    }
+    public class Student : Person
+    {
+        protected int Course;
+        protected int Group;
+        protected string Faculty;
+        protected string University;
+        public Student(string name, string surname, string patronymic, Date birthDate, int course, int group, string faculty, string university) :
+            base(name, surname, patronymic, birthDate)
         {
-            protected int course;
-            protected int group;
-            protected string faculty;
-            protected string university;
+            Course = course;
+            Group = group;
+            Faculty = faculty;
+            University = university;
         }
-        public class Professor : Person
+    }
+    public class Professor : Person
+    {
+        protected string Position;
+        protected string Cathedra;
+        protected string University;
+        public Professor(string name, string surname, string patronymic, Date birthDate, string position, string cathedra, string university) :
+            base(name, surname, patronymic, birthDate)
         {
-            protected string position;
-            protected string cathedra;
-            protected string university;
+            Position = position;
+            Cathedra = cathedra;
+            University = university;
         }
-        public class LibraryUser : Person
+    }
+    public class LibraryUser : Person
+    {
+        protected int ReaderTicket;
+        protected Date IssueDate;
+        protected int MonthlyContribution;
+        public LibraryUser(string name, string surname, string patronymic, Date birthDate, int readerTicket, Date issueDate, int monthlyContribution) :
+            base(name, surname, patronymic, birthDate)
         {
-            protected int readerTicket;
-            protected Date issueDate;
-            protected int monthlyContribution;
+            ReaderTicket = readerTicket;
+            IssueDate = issueDate;
+            MonthlyContribution = monthlyContribution;
         }
     }
 }
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -3,6 +3,9 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Enrollee enrollee = new Enrollee("Олександр", "Коваленко", "Іванович", new Date(15, 2, 2006), 150, 10, "Приклад");
+        string name = enrollee.GetName();
+        Console.WriteLine(name);
     }
 }
